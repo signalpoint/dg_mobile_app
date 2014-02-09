459367db228dd7875cce75a176eefb5ab052b669
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-02-09 15:48:15 -0500

    Removed all the old deprecated pages and javascript files.

--- www/DrupalGap/app/modules/custom/dg_pro/README.md
+++
@@ -1,4 +0,0 @@
-dg_pro
-======
-
-A custom module for the DrupalGap Mobile Application Development Kit that powers the DrupalGap mobile application.
--- www/DrupalGap/app/modules/custom/dg_pro/pages/welcome.js
+++
@@ -1,3 +0,0 @@
-$('#dg_welcome_setup').on('click', function(){
-	drupalgap_changePage(drupalgap_get_path('module', 'dg_pro') + '/pages/setup.html');
-});
